ddb1c86879ae37c10b019f9dcd1f5208c44b2f9f
Carlos Benavides <1992cab@gmail.com>
2019-10-22 22:23:26 -0500

    chore: added env variable for mongo

--- server/config/config.js
+++ server/config/config.js
@@ -5,5 +5,5 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 if (process.env.NODE_ENV === 'dev') {
     process.env.DB = 'mongodb://localhost:27017/coffee';
 } else {
-    process.env.DB = 'mongodb+srv://carlosbean:5wa7XGD5sH454Rkx@cluster0-jsfn9.mongodb.net/coffee';
+    process.env.DB = process.env.MONGO_URL;
 }
